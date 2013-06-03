1b890b07ad89ebab706aba786691739765da9053
Geoff Schorkopf <gschorkopf@gmail.com>
2013-06-03 16:43:11 -0600

    Switch up on the scan times

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :badges_json, :badges_url, :badges_gem
+
+  def badges_url
+    "http://eldoorado-api.herokuapp.com/badge_scans.json"
+  end
+
+  def badges_json
+    @badges_json ||= JSON.parse RestClient.get(badges_url)
+  end
+
+  def badges_gem
+    @badges_gem ||= Eldoorado::BadgeScan.all
+  end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,26 +3,37 @@ class DashboardController < ApplicationController
   end
 
   def search
-    @badge_scans = []
+    badge_scans = []
 
     if params[:q]
-      queries = params[:q].gsub("AND", "&").split("OR")
-      queries.each do |query|
-        parsed = Rack::Utils.parse_nested_query query
-
-        scans.each do |scan|
-          validity = []
-          parsed.each_pair do |param, value|
-            if scan[param.strip] && scan[param.strip].downcase.include?(value.downcase.strip)
-              validity << true
-            else
-              validity << false
-            end
-          end
-
-          @badge_scans << scan unless validity.include? false
-        end
+      # query = Rack::Utils.parse_nested_query params[:q]
+      company = Eldoorado::Company.find_by_name(params[:q])
+      company.badge_scans.each do |badge_scan|
+        badge_scans << badge_scan
       end
+
+      @badge_scans = badge_scans.sort_by(&:scan_date).reverse
     end
+
+    @badge_scans
+    # if params[:q]
+    #   queries = params[:q].gsub("AND", "&").split("OR")
+    #   queries.each do |query|
+    #     parsed = Rack::Utils.parse_nested_query query
+
+    #     scans.each do |scan|
+    #       validity = []
+    #       parsed.each_pair do |param, value|
+    #         if scan[param.strip] && scan[param.strip].downcase.include?(value.downcase.strip)
+    #           validity << true
+    #         else
+    #           validity << false
+    #         end
+    #       end
+
+    #       @badge_scans << scan unless validity.include? false
+    #     end
+    #   end
+    # end
   end
 end

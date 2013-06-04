2908eaa4c1b2b766fadf5f8c71a60b6e778fd04d
Geoff Schorkopf <gschorkopf@gmail.com>
2013-06-03 19:30:59 -0600

    Yay tables and such

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,37 +3,26 @@ class DashboardController < ApplicationController
   end
 
   def search
-    badge_scans = []
-
+    scans = badges_gem
+    @badge_scans = []
     if params[:q]
-      # query = Rack::Utils.parse_nested_query params[:q]
-      company = Eldoorado::Company.find_by_name(params[:q])
-      company.badge_scans.each do |badge_scan|
-        badge_scans << badge_scan
+      queries = params[:q].gsub("AND", "&").split("OR")
+      queries.each do |query|
+        parsed = Rack::Utils.parse_nested_query query
+
+        scans.each do |scan|
+          validity = []
+          parsed.each_pair do |param, value|
+            if scan[param.strip] && scan[param.strip].downcase.include?(value.downcase.strip)
+              validity << true
+            else
+              validity << false
+            end
+          end
+
+          @badge_scans << scan unless validity.include? false
+        end
       end
-
-      @badge_scans = badge_scans.sort_by(&:scan_date).reverse
     end
-
-    @badge_scans
-    # if params[:q]
-    #   queries = params[:q].gsub("AND", "&").split("OR")
-    #   queries.each do |query|
-    #     parsed = Rack::Utils.parse_nested_query query
-
-    #     scans.each do |scan|
-    #       validity = []
-    #       parsed.each_pair do |param, value|
-    #         if scan[param.strip] && scan[param.strip].downcase.include?(value.downcase.strip)
-    #           validity << true
-    #         else
-    #           validity << false
-    #         end
-    #       end
-
-    #       @badge_scans << scan unless validity.include? false
-    #     end
-    #   end
-    # end
   end
 end

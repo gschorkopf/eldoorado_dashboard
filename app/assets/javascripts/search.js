6e95c2cfc382aeab1d6f56287d13234fdefc0e04
Geoff Schorkopf <gschorkopf@gmail.com>
2013-06-04 23:34:36 -0600

    Circles working state

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   var badgeScans = gon.badge_scans;
-  
   var doorCoords = {"Atrium Door (In)": {x: 260, y: 360, fill: "green"},
                     "Back Door (In)": {x: 765, y: 215, fill: "red"},
                     "Front Door (In)": {x: 50, y: 725, fill: "purple"},
@@ -13,44 +12,35 @@ $(document).ready(function() {
   var index = 0;
   var scan = null;
 
-
   function getNext() {
     scan = badgeScans[index];
     console.log("got scan " + scan);
 
     if (scan) {
-      var scanAction = svgContainer.selectAll("g")
-        .data([scan], function(d){return d.scan_time});
+      var scanCircle = svgContainer.selectAll("circle.scan")
+        .data([scan], function(d) { return d.scan_time });
 
-      scanAction.exit().remove();
-
-      var group = scanAction
+      scanCircle.exit().remove();
+      
+      scanCircle
         .enter()
-        .append("g");
-
-      group
         .append('circle')
         .attr('class', 'scan')
         .attr('r', 20)
-        .style("fill", function(d){return doorCoords[d.door].fill;});
-
-      group
-        .append("text")
-        .text(function(d){return d.first_name});
-        
-      group
-        .style("fill", "blue")
-        .attr("dx", function(d){
-          console.log("entered through door: " + d.door + " at " + d.scan_time);
-          return doorCoords[d.door].x;
+        .text(function(d){return d.first_name;})
+        .attr("cx", function(d){
+        console.log("entered through door: " + d.door + " at " + d.scan_time);
+        return doorCoords[d.door].x;
         })
-        .attr("dy", function(d){return doorCoords[d.door].y;})
+        .attr("cy", function(d){return doorCoords[d.door].y;})
+        .style("fill", function(d){return doorCoords[d.door].fill;})
         .transition()
         .duration(1000)
-        .attr("dx", function(d){return compCoords[d.company].x;})
-        .attr("dy", function(d){return compCoords[d.company].y;});
+        .attr("cx", function(d){return compCoords[d.company].x;})
+        .attr("cy", function(d){return compCoords[d.company].y;})
 
       window.setTimeout(getNext, 2000);
+
       index++;
     };
   };

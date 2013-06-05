10b034e73f1b45c1b51b6f3b9116dfcc906c3e5c
Geoff Schorkopf <gschorkopf@gmail.com>
2013-06-05 13:55:08 -0600

    Some odd refactors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,36 @@ class ApplicationController < ActionController::Base
   def badges_gem
     @badges_gem ||= Eldoorado::BadgeScan.all
   end
+
+  def return_badge_scans_from_params(params)
+    badge_scans = []
+    queries = params.gsub("AND", "&").split("OR")
+    queries.each do |query|
+      parsed = Rack::Utils.parse_nested_query query
+      badges_gem.each do |scan|
+        badge_scans << scan unless scan_has_any_invalid_params(scan, parsed)
+      end
+    end
+    return sorted_asc_by_date(badge_scans)
+  end
+
+  def scan_has_any_invalid_params(scan, parsed)
+    validity = []
+    parsed.each_pair do |param, value|
+      if scan[param.strip] && scan_includes_value(scan, param, value)
+        validity << true
+      else
+        validity << false
+      end
+    end
+    validity.include? false
+  end
+
+  def sorted_asc_by_date(badge_scans)
+    badge_scans.sort_by! {|scan| scan.scan_time }
+  end
+
+  def scan_includes_value(scan, param, value)
+    scan[param.strip].downcase.include?(value.downcase.strip)
+  end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,29 +4,13 @@ class DashboardController < ApplicationController
   end
 
   def search
-    scans = badges_gem
     @badge_scans = []
+
     if params[:q]
       @params = params[:q]
-      queries = params[:q].gsub("AND", "&").split("OR")
-      queries.each do |query|
-        parsed = Rack::Utils.parse_nested_query query
-
-        scans.each do |scan|
-          validity = []
-          parsed.each_pair do |param, value|
-            if scan[param.strip] && scan[param.strip].downcase.include?(value.downcase.strip)
-              validity << true
-            else
-              validity << false
-            end
-          end
-
-          @badge_scans << scan unless validity.include? false
-        end
-      end
+      @badge_scans = return_badge_scans_from_params(params[:q])
     end
-    @badge_scans.sort_by! {|scan| scan.scan_time }
+
     gon.badge_scans = @badge_scans
   end
 end

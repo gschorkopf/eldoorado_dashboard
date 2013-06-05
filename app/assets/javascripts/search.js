361f45077b9bdee7489738c87f1f11cead98fc2d
Geoff Schorkopf <gschorkopf@gmail.com>
2013-06-04 18:22:52 -0600

    Mike additions

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,49 +1,17 @@
 $(document).ready(function() {
   // var badgeScans = gon.badge_scans;
-  // debugger
-  var badgeScans = [{company: "Student",
-                    door: "Atrium Door (In)",
-                    first_name: "Geoffrey",
-                    guest: false,
-                    last_name: "Schorkopf",
-                    scan_time: "11111"},
-                    {company: "Roximity",
-                    door: "Front Door (In)",
-                    first_name: "Geoffrey",
-                    guest: false,
-                    last_name: "Schorkopf",
-                    scan_time: "22222"},
-                    {company: "gSchool",
-                    door: "Back Door (In)",
-                    first_name: "Geoffrey",
-                    guest: false,
-                    last_name: "Schorkopf",
-                    scan_time: "33333"}
-                    ];
 
+  var badgeScans = gon.badge_scans;
   
   var doorCoords = {"Atrium Door (In)": {x: 260, y: 360, fill: "green"},
                     "Back Door (In)": {x: 765, y: 215, fill: "red"},
-                    "Front Door (In)": {x: 50, y: 725, fill: "purple"}};
-                    // {x: 50, y: 300, door: "Knoll Door (In)", fill: "blue"}];
+                    "Front Door (In)": {x: 50, y: 725, fill: "purple"},
+                    "Knoll Door (In)": {x: 50, y: 300, fill: "blue"}};
   var compCoords = {"Student": {x: 400, y: 400},
                     "gSchool": {x: 300, y: 500},
                     "Roximity": {x: 800, y: 200}};
 
   var svgContainer = d3.select("#svg");
-
-  // var doors = svgContainer
-  //   .selectAll("circle")
-  //   .data(doorCoords);
-  
-  // doors.enter()
-  //   .append("circle")
-  //   .text(function(d){return d.door})
-  //   .attr("cx", function(d){return d.x;})
-  //   .attr("cy", function(d){return d.y;})
-  //   .attr("r", 15)
-  //   .style("fill", function(d){return d.fill;});
-
   var index = 0;
   var scan = null;
 
@@ -65,7 +33,7 @@ $(document).ready(function() {
       .attr('r', 20)
       .text(function(d){return d.first_name;})
       .attr("cx", function(d){
-        console.log("entered through door: " + d.door);
+        console.log("entered through door: " + d.door + " at " + d.scan_time);
         return doorCoords[d.door].x;
       })
       .attr("cy", function(d){return doorCoords[d.door].y;})

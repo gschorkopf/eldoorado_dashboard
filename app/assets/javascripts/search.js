298dc35544a47a2f1dce0c89daed93a3c53e4a6c
Geoff Schorkopf <gschorkopf@gmail.com>
2013-06-04 23:34:01 -0600

    Tried to make text work

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -19,30 +19,38 @@ $(document).ready(function() {
     console.log("got scan " + scan);
 
     if (scan) {
+      var scanAction = svgContainer.selectAll("g")
+        .data([scan], function(d){return d.scan_time});
 
-      var scanCircle = svgContainer.selectAll("circle.scan")
-      .data([scan], function(d) { return d.scan_time });
+      scanAction.exit().remove();
 
-      scanCircle.exit().remove();
-      scanCircle
-      .enter()
-      .append('circle')
-      .attr('class', 'scan')
-      .attr('r', 20)
-      .text(function(d){return d.first_name;})
-      .attr("cx", function(d){
-        console.log("entered through door: " + d.door + " at " + d.scan_time);
-        return doorCoords[d.door].x;
-      })
-      .attr("cy", function(d){return doorCoords[d.door].y;})
-      .style("fill", function(d){return doorCoords[d.door].fill;})
-      .transition()
-      .duration(1000)
-      .attr("cx", function(d){return compCoords[d.company].x;})
-      .attr("cy", function(d){return compCoords[d.company].y;})
+      var group = scanAction
+        .enter()
+        .append("g");
+
+      group
+        .append('circle')
+        .attr('class', 'scan')
+        .attr('r', 20)
+        .style("fill", function(d){return doorCoords[d.door].fill;});
+
+      group
+        .append("text")
+        .text(function(d){return d.first_name});
+        
+      group
+        .style("fill", "blue")
+        .attr("dx", function(d){
+          console.log("entered through door: " + d.door + " at " + d.scan_time);
+          return doorCoords[d.door].x;
+        })
+        .attr("dy", function(d){return doorCoords[d.door].y;})
+        .transition()
+        .duration(1000)
+        .attr("dx", function(d){return compCoords[d.company].x;})
+        .attr("dy", function(d){return compCoords[d.company].y;});
 
       window.setTimeout(getNext, 2000);
-
       index++;
     };
   };
